d2fe0ab7a3a9e5a2361106f1b573ce540d2bbf98
mridul-joshi <mriduljoshimj12@gmail.com>
2021-09-02 12:00:26 +0530

    added pipes

--- flappyBird.py
+++ flappyBird.py
@@ -3,6 +3,8 @@ import neat
 import time
 import os
 import random
+pygame.font.init()
+
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
@@ -22,6 +24,8 @@ BASE_IMG = pygame.transform.scale2x(
 
 BG_IMG = pygame.transform.scale2x(
     pygame.image.load(os.path.join("imgs", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont('comicsans', 50)
 
 
 class Bird:
@@ -185,18 +189,29 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base, score):
     # blit=draw
     win.blit(BG_IMG, (0, 0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render('Score: '+str(score), 1, (255, 255, 255))
+    win.blit(text, (WIN_WIDTH-10-text.get_width(), 10))
+
+    base.draw(win)
+
     bird.draw(win)
     pygame.display.update()
 
 
 def main():
 
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-
+    score = 0
     clock = pygame.time.Clock()
 
     run = True
@@ -206,8 +221,36 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        bird.move()
-        draw_window(win, bird)
+        # bird.move()
+        add_pipe = False
+        rem = []
+
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+
+            if pipe.x+pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+
+            pipes.append(Pipe(600))
+
+        for r in rem:
+            pipes.remove(r)
+
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+
+        base.move()
+        draw_window(win, bird, pipes, base, score)
 
     pygame.quit()
     quit()
